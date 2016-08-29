9c45fd2c7ec196d704c1d42deac8e2f8ef7e272a
John <hcjiang@issp.ac.cn>
2016-08-29 16:27:08 +0800

    建立基本的模型-视图-控制器结构

---
+++ controller.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from PyQt4 import QtGui
+
+import model
+import requisition_template
+
+
+class RequisitionWindow(QtGui.QWidget):
+    def __init__(self):
+        super(RequisitionWindow, self).__init__()
+        self.ui = requisition_template.Ui_Form()
+        self.ui.setupUi(self)
+        self.requisition = model.PurchaseRequisition()
+        self._update_model()
+
+    def _update_model(self):
+        self.requisition.applicant = self.ui.comboBox_applicant.currentText()
+
+
+if __name__ == '__main__':
+    app = QtGui.QApplication([])
+    app.setFont(QtGui.QFont('Trebuchet MS', 9))
+    w = RequisitionWindow()
+    w.show()
+    app.exec_()
--- model.py
+++ model.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
+
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 
 engine = create_engine('sqlite:///model.sqlite')
-BaseModel = declarative_base()
+Model = declarative_base()
 
 
-class User(BaseModel):
+class User(Model):
     __tablename__ = "user"
     id = Column(Integer, primary_key=True)
     name = Column(String)
 
 
-class ResearchProject(BaseModel):
+class ResearchProject(Model):
     __tablename__ = "project"
     id = Column(Integer, primary_key=True)
     name = Column(String)
@@ -22,8 +23,8 @@ class ResearchProject(BaseModel):
     user = relationship(User)
 
 
-class PurchaseRequisiton(BaseModel):
-    __tablename__ = "purchase"
+class PurchaseRequisition(Model):
+    __tablename__ = "requisition"
     id = Column(Integer, primary_key=True)
     title = Column(String)
     date = Column(Date, default=func.now())
@@ -38,28 +39,27 @@ class PurchaseRequisiton(BaseModel):
     review_group = Column(String)
 
 
-class Contract(BaseModel):
+class Contract(Model):
     __tablename__ = "contract"
     id = Column(Integer, primary_key=True)
 
 
 session = sessionmaker()
 session.configure(bind=engine)
-BaseModel.metadata.create_all(engine)
-s = session()
-user1 = User(name=u'蒋华超')
-user2 = User(name=u'赵钱孙')
+Model.metadata.create_all(engine)
+
+# user1 = User(name=u'蒋华超')
+# user2 = User(name=u'赵钱孙')
 # s.add(user1)
 # s.add(user2)
-project = ResearchProject(name=u"高压超快光学平台", user=user1)
+# project = ResearchProject(name=u"高压超快光学平台", user=user1)
 # s.add(project)
-p = PurchaseRequisiton(title=u"太赫兹光谱系统", purchase_type=u"公开招标", applicant=user1, project=project)
+# p = PurchaseRequisition(title=u"太赫兹光谱系统", purchase_type=u"公开招标", applicant=user1, project=project)
 # s.add(p)
-s.commit()
-users = s.query(User).all()
-for u in users:
-    print u.name, u.id
-purchases = s.query(PurchaseRequisiton).all()
-for p in purchases:
-    print p.title, p.date, p.applicant.name, p.applicant_id, p.project.name
-s.close()
+# s.commit()
+# users = s.query(User).all()
+# for u in users:
+#     print u.name, u.id
+# purchases = s.query(PurchaseRequisition).all()
+# for p in purchases:
+#     print p.title, p.date, p.applicant.name, p.applicant_id, p.project.name
