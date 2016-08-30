7b25ef4e046cafe2c8f1352a398f48eff4f315a7
John <hcjiang@issp.ac.cn>
2016-08-30 16:31:07 +0800

    建立基本的模型-视图-控制器结构

--- controller.py
+++ controller.py
@@ -12,13 +12,47 @@ class RequisitionWindow(QtGui.QWidget):
         super(RequisitionWindow, self).__init__()
         self.ui = requisition_template.Ui_Form()
         self.ui.setupUi(self)
-        self.staff = model.Person()
-        self._update_model()
+        self._initialize()
+        self.ui.toolButton_reason_gen.clicked.connect(self._update_model)
+
+    def _initialize(self):
+        names = [u'苏付海', u'刘晓迪', u'杨雪', u'姜树清', u'丁俊峰', u'王媛', u'蒋华超']
+        names.sort()
+        for n in names:
+            print n
+        staffs = session.query(model.Staff).all()
+        for s in staffs:
+            self.ui.comboBox_applicant.addItem(s.name)
 
     def _update_model(self):
-        staffs = session.query(model.Person).all()
-        for i in xrange(len(staffs)):
-            print staffs[i].name, staffs[i].id
+        request_title = unicode(self.ui.lineEdit_request_title.text())
+        request_applicant = unicode(self.ui.comboBox_applicant.currentText())
+        request_date = unicode(self.ui.dateEdit_request_date.date())
+        purchase_type = unicode(self.ui.comboBox_purchase_type.currentText())
+        grant_number = unicode(self.ui.comboBox_project.currentText())
+        purchase_method = unicode(self.ui.comboBox_purchase_method.currentText())
+        is_budget = self.ui.radioButton_yes.isChecked()  # True/False
+        if is_budget:
+            budget_value = self.ui.doubleSpinBox_budget.value()
+            budget = self.ui.doubleSpinBox_budget.text()
+        else:
+            budget = ''
+        purchase_reason = unicode(self.ui.lineEdit_purchase_reason.text())
+        tech_specification = unicode(self.ui.lineEdit_tech_spec.text())
+
+        staffs = session.query(model.Staff).filter_by(name=request_applicant).all()
+        if len(staffs) == 0:
+            raise u'严重错误！未在数据库中找到选择的申请人，请联系系统管理员。'
+
+        projects = session.query(model.Project).filter_by(grant_number=grant_number).all()
+        if len(projects) == 0:
+            raise u'严重错误！未在数据库中找到选择的课题号，请联系系统管理员。'
+
+        requisition = Requisition(title=request_title, applicant=staffs[0], project=projects[0],
+                                  purchase_method=u'公开招标',
+                                  purchase_reason=u'该项目是中科院修购专项批准购买的专项设备。')
+        session.add(requisition)
+        session.commit()
 
 
 if __name__ == '__main__':
@@ -26,4 +60,5 @@ if __name__ == '__main__':
     app.setFont(QtGui.QFont('Trebuchet MS', 9))
     window = RequisitionWindow()
     window.show()
+    app.setStyle('windowsxp')
     app.exec_()
--- model.py
+++ model.py
@@ -6,60 +6,86 @@ from sqlalchemy.orm import relationship, sessionmaker
 
 engine = create_engine('sqlite:///model.sqlite', echo=False)
 Model = declarative_base()
-Model.metadata.create_all(engine)
-Session = sessionmaker()
-Session.configure(bind=engine)
-session = Session()
 
 
-class Person(Model):
-    __tablename__ = "person"
+class Staff(Model):
+    __tablename__ = "staff"
     id = Column(Integer, primary_key=True)
     name = Column(String)
 
 
-class ResearchProject(Model):
+class Project(Model):
     __tablename__ = "project"
     id = Column(Integer, primary_key=True)
+    grant_number = Column(String, unique=False)
     name = Column(String)
-    type = Column(String)
-    user_id = Column(Integer, ForeignKey(Person.id))
-    user = relationship(Person)
+    grant_type = Column(String)
+    leader_id = Column(Integer, ForeignKey(Staff.id))
+    leader = relationship(Staff)
 
 
-class PurchaseRequisition(Model):
+class Requisition(Model):
     __tablename__ = "requisition"
     id = Column(Integer, primary_key=True)
     title = Column(String)
     date = Column(Date, default=func.now())
-    applicant_id = Column(Integer, ForeignKey(Person.id))
-    applicant = relationship(Person)
+    applicant_id = Column(Integer, ForeignKey(Staff.id))
+    applicant = relationship(Staff)
     purchase_type = Column(String)
-    project_id = Column(Integer, ForeignKey(ResearchProject.id))
-    project = relationship(ResearchProject)
+    project_id = Column(Integer, ForeignKey(Project.id))
+    project = relationship(Project)
     purchase_method = Column(String)
     purchase_reason = Column(String)
-    qualification = Column(String)
-    review_group = Column(String)
+    tech_specification = Column(String)
 
 
 class Contract(Model):
     __tablename__ = "contract"
     id = Column(Integer, primary_key=True)
 
-    # from contextlib import contextmanager
-    #
-    # @contextmanager
-    # def session_scope():
-    #     session = Session()
-    #     try:
-    #         yield session
-    #         session.commit()
-    #     except:
-    #         session.rollback()
-    #         raise
-    #     finally:
-    #         session.close()
-    # with session_scope() as session:
-    #     all = session.query(Person).all()
-    #     return all
+
+Model.metadata.create_all(engine)
+Session = sessionmaker()
+Session.configure(bind=engine)
+session = Session()
+
+
+def generate_test_db():
+    su_fu_hai = Staff(name=u'苏付海')
+    liu_xiao_di = Staff(name=u'刘晓迪')
+    yang_xue = Staff(name=u'杨雪')
+    jiang_shu_qing = Staff(name=u'姜树清')
+    ding_jun_feng = Staff(name=u'丁俊峰')
+    wang_yuan = Staff(name=u'王媛')
+    jiang_hua_chao = Staff(name=u'蒋华超')
+    names = [u'苏付海', u'刘晓迪', u'杨雪', u'姜树清', u'丁俊峰', u'王媛', u'蒋华超']
+
+    p1 = Project(grant_number='Y64NLXG362', name=u'极端高压超快光学实验平台', grant_type=u'中科院修购专项', leader=su_fu_hai)
+    p2 = Project(grant_number='Y64NLYZ251', name=u'高温高压下热导率原位测量', grant_type=u'中科院科研装备研制项目', leader=su_fu_hai)
+    p3 = Project(grant_number='Y64NLXG363', name=u'极端条件下新奇化学态的探索', grant_type=u'中科院修购专项', leader=su_fu_hai)
+    req = Requisition(title=u'实验室办公用品采购申请测试用例一', applicant=jiang, project=p1, purchase_method=u'公开招标',
+                      purchase_reason=u'该项目是中科院修购专项批准购买的专项设备。')
+    session.add_all([su_fu_hai, jiang, temp])
+    session.add(p1)
+    session.add(req)
+    session.commit()
+
+
+def test_db():
+    staffs = session.query(Staff).all()
+    for s in staffs:
+        print s.id, s.name
+    reqisitions = session.query(Requisition).filter_by(title=u'实验室办公用品采购申请测试用例一').all()
+    if len(reqisitions) > 0:
+        req = reqisitions[0]
+        print req.applicant.id, req.applicant.name, req.project.name, req.project.leader.name, req.project.grant_number, req.project.id
+        projects = session.query(Project).all()
+        for p in projects:
+            print p.id, p.name, p.grant_number
+    session.commit()
+
+
+s = session.query(Staff).all()
+if len(s) == 0:
+    generate_test_db()
+# test_db()
