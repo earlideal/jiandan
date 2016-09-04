91df185f63e44aacf4c4e8637dd05d5ae22033a2
Huachao <earlideal@gmail.com>
2016-09-04 21:31:13 +0800

    添加验收文件

---
+++ acceptance_controller.py
@@ -0,0 +1,10 @@
+from PyQt4 import QtGui
+
+import acceptance_template
+
+
+class AcceptanceWindow(QtGui.QWidget):
+    def __init__(self):
+        super(AcceptanceWindow, self).__init__()
+        self.ui = acceptance_template.Ui_Form()
+        self.ui.setupUi(self)
--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,7 @@
 from PyQt4 import QtGui
 
 import dashboard_template
+from acceptance_controller import AcceptanceWindow
 from contract_controller import ContractWindow
 from inventory_controller import InventoryWindow
 from requisition_controller import RequisitionWindow
@@ -14,16 +15,18 @@ class MainWindow(QtGui.QMainWindow):
 
         self.reqisition_window = RequisitionWindow()
         self.contract_window = ContractWindow()
+        self.acceptance_window = AcceptanceWindow()
         self.inventory_window = InventoryWindow()
         self.ui.verticalLayout_requisition_tab.addWidget(self.reqisition_window)
         self.ui.verticalLayout_contract_tab.addWidget(self.contract_window)
+        self.ui.verticalLayout_acceptance_tab.addWidget(self.acceptance_window)
         self.ui.horizontalLayout_inventory.addWidget(self.inventory_window)
 
 
 if __name__ == '__main__':
     app = QtGui.QApplication([])
     app.setFont(QtGui.QFont('trebuchet ms', 9))
-    app.setStyle("windows")
+    app.setStyle("plastique")
     window = MainWindow()
     window.showMaximized()
     app.exec_()
