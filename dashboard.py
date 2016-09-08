6b2057f10da337778917216773b634d26d6e284b
Huachao <earlideal@gmail.com>
2016-09-08 23:25:41 +0800

    更改程序结构

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from PyQt4 import QtGui
 
 from transaction import TransactionWidget
@@ -11,7 +12,7 @@ class DashboardWindow(QtGui.QMainWindow):
         self.ui.setupUi(self)
 
         self.transaction_widget = TransactionWidget()
-        self.ui.horizontalLayout_content.addWidget(self.transaction_widget)
+        # self.ui.horizontalLayout_content.addWidget(self.transaction_widget)
         # 如果想要从布局中移除控件，必须在移除后再调用deleteLater方法，否则控件会自由浮动，但不会消失
-        self.ui.horizontalLayout_content.removeWidget(self.transaction_widget)
-        self.transaction_widget.deleteLater()
+        # self.ui.horizontalLayout_content.removeWidget(self.transaction_widget)
+        # self.transaction_widget.deleteLater()
