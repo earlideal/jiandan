6ae6cc2c0fd71676aa44da7ad84e0990911078a6
John <hcjiang@issp.ac.cn>
2016-09-12 16:34:06 +0800

    word

--- tables/word.py
+++ tables/word.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
-
+import os
+import time
 from win32com.client import Dispatch
 
 word = Dispatch('Word.Application')
 word.Visible = 1
 word.DisplayAlerts = 0
-path = 'C:\\Users\\John\\PycharmProjects\\jiandan\\templates\\' + '请购单.docx'
-print path
-doc = word.Documents.Open(path)
-word.ActiveDocument.Save()
-word.ActiveDocument.PrintOut()
-word.ActiveDocument.Close()
+path = 'C:\\Users\\John\\Documents\\PYTHON\\jiandan\\templates\\'
+for file in os.listdir(path):
+    print file.decode(encoding='gb2312')
+    doc = word.Documents.Open(path + "\\" + file)
+    word.ActiveDocument.Save()
+    # word.ActiveDocument.PrintOut()
+    word.ActiveDocument.Close()
 word.Quit()
+class WordProcessor(object):
+    def __init__(self):
+        super(WordProcessor, self).__init__()
